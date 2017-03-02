8f80dde16b5cf0e4eafcfcc011bbfbf9fc9c597f
davit <davstepanyan95@gmail.com>
2017-03-03 00:58:24 +0400

    user module

--- frontend/modules/user/views/user/login.php
+++ frontend/modules/user/views/user/login.php
@@ -1,8 +1,6 @@
 <?php
 $this->pageTitle=Yii::app()->name . ' - '.UserModule::t("Login");
-$this->breadcrumbs=array(
-	UserModule::t("Login"),
-);
+
 ?>
 
 <h1><?php echo UserModule::t("Login"); ?></h1>
